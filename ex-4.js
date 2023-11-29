65f7ef4ce027622742a89933b6574730abc504b5
tanasri <tanawan95@gmail.com>
2023-11-29 09:37:40 +0700

    completed exercise

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,23 @@
 // Start coding here
-let calculator = {};
+
+let calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  subtract: (a, b) => {
+    return a - b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+const addResult = calculator.add;
+console.log(addResult(10, 20));
+
+const divideResult = calculator.divide;
+console.log(divideResult(3000, 10));
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,8 @@
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
+
+function findMinNumber(a, b) {
+  return Math.min(a, b);
+}
+console.log(minNumber);
